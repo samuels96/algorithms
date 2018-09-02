5e2b90b5323a7c50d1775634a1a2a20049e97698
samuels96 <37853844+samuels96@users.noreply.github.com>
2018-09-02 12:19:10 +0200

    Update recursive_quicksort.py

--- recursive_quicksort.py
+++ recursive_quicksort.py
@@ -2,7 +2,9 @@ def quicksort(x):
     if len(x) < 2: return x
 
     piv = x[len(x)//2]
+    
     del x[len(x)//2]
+    
     less = [x for x in x if x < piv]
     more = [x for x in x if x > piv]
 
