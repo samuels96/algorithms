52eac95f39c20b7c95add1ae536cb6c0937f8cab
samuels96 <37853844+samuels96@users.noreply.github.com>
2018-09-15 11:33:53 +0200

    Update bubble_sort.py

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -3,8 +3,13 @@
 #Space complexity: O(1)
 
 def bubble_sort(arr):
-    for _ in range(len(arr)-1):
+    is_sorted = False
+
+    while not is_sorted:
+        is_sorted = True
+
         for i in range(len(arr)-1):
-            if arr[i]>arr[i+1]:
-                arr[i+1],arr[i] = arr[i],arr[i+1]
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                is_sorted = False
     return arr
